85cb55cfc81ed806b8167e2f82212f9b61d7d26e
lynn-baydoun <aaz29@mail.aub.edu>
2023-12-21 15:14:51 +0200

    setting-folder-structure

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { About, Skills,Footer, Header, Testimonials, Work } from "./containers";
+import {Navbar} from "./components"
+import "./App.scss";
+
+const App = () =>{
+    return (
+        <div className="app">
+            <Navbar/>
+            <Header/>
+            <About/>
+            <Work/>
+            <Skills/>
+            <Testimonials/>
+            <Footer/>
+        </div>
+
+    )
+}
+
+export default App; 
