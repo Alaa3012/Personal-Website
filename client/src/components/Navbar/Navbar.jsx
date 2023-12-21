8ecb6503a8db8c46466e1e7aede33b3080edb39d
lynn-baydoun <aaz29@mail.aub.edu>
2023-12-22 00:11:19 +0200

    feat : created-navbar-component

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-import "./Navbar.scss"
+import navbarItems from '../../configs/navbar.configs';
+import { images } from '../../constants';
+import "./Navbar.scss";
+import {HiMenuAlt4, HiX} from 'react-icons/hi'; 
+import { motion } from "framer-motion"
+
 
 function Navbar() {
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <nav className='app__navbar'>
+      
+      <div className='app__navbar-logo'>
+        <img src = {images.logo} alt ="logo"/>
+      </div>
+
+      <ul className='app__navbar-links'>
+
+        {navbarItems.map((item)=>(
+          <li className="app__flex p-text" key ={item.link}>
+            <div/>
+            <a href ={item.path}>{item.display}</a>
+          </li>
+        ))}
+
+      </ul>
+
+      <div className = "app__navbar-menu">
+        <HiMenuAlt4 onClick= { ()=> setToggle(true)}/>
+        {toggle && (
+          <motion.div 
+            whileInView ={ {x:[300,0]}} 
+            transition={{duration : 0.85, ease : 'easeOut'}}
+          >
+              <HiX onClick={()=> setToggle(false)}/>
+                <ul>
+                  {navbarItems.map( (item) => (
+                    <li key ={item.display}>
+                      <a href ={item.path} onClick={ ()=> setToggle(false) }>{item.display}</a>
+                    </li>
+                   ))}
+                   <li>test</li>
+                </ul>
+          </motion.div>
+        )}
+      </div>
+    </nav>
   )
 }
 
---
+++ client/src/configs/navbar.configs.js
@@ -0,0 +1,29 @@
+const navbarItems = [
+    {
+        display : 'Home',
+        link : 'link-home',
+        path : '#home'
+    },
+    {
+        display : 'About',
+        link : 'link-about',
+        path : '#about'
+    },
+    {
+        display : 'Contact',
+        link : 'link-contact',
+        path : '#contact'
+    },
+    {
+        display : 'Work',
+        link : 'link-work',
+        path : '#work'
+    },
+    {
+        display : 'Skills',
+        link : 'link-skills',
+        path : '#skills'
+    }
+]
+
+export default navbarItems;
